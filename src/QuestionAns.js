451d8fc2d62947702ae42b7f60c90343668d28d8
yeti103 <sanskarsharma987@gmail.com>
2023-08-13 23:03:15 +0530

    uploading to git hub

--- src/QuestionAns.js
+++ src/QuestionAns.js
@@ -1,29 +1,21 @@
-import React, {useMemo,useState} from 'react'
 import "./QuestionAns.css"
-import axios from 'axios'
 
-function QuestionAns() {
-  const[data,setData]=useState()
+
+function QuestionAns({question,ans0}) {
+ 
   
-  useMemo(()=>{
-    axios.get("https://opentdb.com/api.php?amount=10")
-    .then((res)=>setData(res.data.results))
-  },[])
+  
   
  
   
-  if(!data)
-  {
-    return <div>loading</div>
-  }
+  
   return(
-    <div>
-      < div className="question">{data.map((item)=>{
-        return (<>
-        <div>{item.question}</div>
-        <div>{item.correct_answer}</div>
-        </>)
-        })}</div> 
+    <div className="QuestionAns">
+      < div className="question">
+        <div>{question}</div>
+        <div>{ans0}</div>
+        
+        </div> 
       
     </div>
   )
--- src/QuizPage.js
+++ src/QuizPage.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React ,{useState,useMemo} from 'react'
 import "./QuizPage.css"
 import QuestionAns from './QuestionAns'
+import axios from 'axios'
 function QuizPage() {
+   const[data,setData]=useState()
+   
+   useMemo(()=>{
+    axios.get("https://opentdb.com/api.php?amount=10&category=23")
+    .then((res)=>setData(res.data.results))
+  },[])
+  console.log("data",data);
+  if(!data)
+  {
+    return <div>loading</div>
+  }
 return (
     <div className='QuizPage'>
-      <QuestionAns/>
+      {data.map((item)=>{
+        return <QuestionAns question={item.question} ans0={item.correct_answer}/>})}
+      
     </div>
   )
 }
